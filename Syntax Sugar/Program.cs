7dc7fe1ba1fcb89ee2bd8b5ead8b7fd6dda43eb6
RashellChandler <rashell.chandler@yahoo.com>
2023-09-09 17:16:16 -0500

    first commit

---
+++ Syntax Sugar/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+{
+
+    //int answer = 4; //explicit typing
+    //string response; //explicit typing
+
+    //if (answer < 9) ;
+    {
+       //response = answer + " is less than nine";
+    }
+    //else
+        //response = answer + "greater than or equal to nine";
+
+    {
+        var answer = 4;
+        var response = (answer < 9) ? $"{answer} is less than 9" : $"{answer} is greater than or equal to nine";
+    }
+}
+
